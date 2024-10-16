02724e7a245501511d255841d3916764469553e4
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-10-16 13:05:05 +0300

    .

---
+++ src/main/java/com/tugalsan/api/file/pdf/openpdf/server/TS_FilePdfOpenPdfUtils.java
@@ -0,0 +1,10 @@
+package com.tugalsan.api.file.pdf.openpdf.server;
+
+import com.tugalsan.api.log.server.TS_Log;
+
+public class TS_FilePdfOpenPdfUtils {
+
+    final private static TS_Log d = TS_Log.of(TS_FilePdfOpenPdfUtils.class);
+
+    //    private static String GOOGLEPDF () "https://docs.google.com/gview?url=";
+}
--- src/main/java/com/tugalsan/api/file/pdf/openpdf/server/TS_FilePdfOpenPdfUtilsFont.java
+++ src/main/java/com/tugalsan/api/file/pdf/openpdf/server/TS_FilePdfOpenPdfUtilsFont.java
@@ -4,17 +4,28 @@ import com.lowagie.text.Font;
 import com.lowagie.text.FontFactory;
 import com.lowagie.text.pdf.BaseFont;
 import com.lowagie.text.pdf.LayoutProcessor;
+import java.awt.Color;
 import java.nio.file.Path;
 
 public class TS_FilePdfOpenPdfUtilsFont {
 
-    public static void enable() {
-        LayoutProcessor.enableKernLiga();
-    }
+    private static volatile boolean WARMED_UP = false;
 
-    public static Font craete(Path ttf, String alias, float size, boolean left_to_right) {
+    public static Font craete(Path ttf, String alias, float size, boolean left_to_right, boolean bold, boolean italic, Color color) {
+        if (!WARMED_UP) {
+            WARMED_UP = true;
+            LayoutProcessor.enableKernLiga();
+        }
+        var style = Font.NORMAL;
+        if (bold && italic) {
+            style = Font.BOLDITALIC;
+        } else if (bold) {
+            style = Font.BOLD;
+        } else if (italic) {
+            style = Font.ITALIC;
+        }
         FontFactory.register(ttf.toAbsolutePath().toString(), alias);
-        var font = FontFactory.getFont(alias, BaseFont.IDENTITY_H, true, size);
+        var font = FontFactory.getFont(alias, BaseFont.IDENTITY_H, true, size, style, color);
         if (left_to_right) {
             LayoutProcessor.setRunDirectionLtr(font);
         } else {
