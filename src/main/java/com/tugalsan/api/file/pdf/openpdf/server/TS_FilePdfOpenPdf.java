0b60a7166baed8c75e0dd77c25fe21e4e445bb54
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-10-21 11:26:17 +0300

    Update TS_FilePdfOpenPdf.java

--- src/main/java/com/tugalsan/api/file/pdf/openpdf/server/TS_FilePdfOpenPdf.java
+++ src/main/java/com/tugalsan/api/file/pdf/openpdf/server/TS_FilePdfOpenPdf.java
@@ -27,7 +27,7 @@ import java.util.stream.IntStream;
 //https://web.archive.org/web/20090703162811/http://itextdocs.lowagie.com/tutorial/
 public class TS_FilePdfOpenPdf extends TS_FileCommonAbstract {
 
-    final private static TS_Log d = TS_Log.of(true, TS_FilePdfOpenPdf.class);
+    final private static TS_Log d = TS_Log.of(false, TS_FilePdfOpenPdf.class);
     public TS_FilePdfOpenPdfUtils pdf;
     public PdfPTable pdfTable = null;
     public PdfPCell pdfCell = null;
