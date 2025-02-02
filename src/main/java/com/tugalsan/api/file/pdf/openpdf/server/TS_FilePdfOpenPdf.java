1b6ef64f487f008573c7f6a63070b35e7ef01f7f
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-02-02 08:41:43 +0300

    .

--- src/main/java/com/tugalsan/api/file/pdf/openpdf/server/TS_FilePdfOpenPdf.java
+++ src/main/java/com/tugalsan/api/file/pdf/openpdf/server/TS_FilePdfOpenPdf.java
@@ -29,6 +29,11 @@ import java.util.stream.IntStream;
 public class TS_FilePdfOpenPdf extends TS_FileCommonAbstract {
 
     final private static TS_Log d = TS_Log.of(false, TS_FilePdfOpenPdf.class);
+
+    public String getSuperClassName() {
+        return d.className;
+    }
+
     public TS_FilePdfOpenPdfUtils pdf;
     public PdfPTable pdfTable = null;
     public PdfPCell pdfCell = null;
